ffeaa96c27b10ace4e2ce9cf8a3d5f5143295e5c
Adityarajsinh Zala <adityarajza@gmail.com>
2025-02-01 20:45:15 +0530

    Added all files

---
+++ src/command.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Flags struct {
+	Add    string
+	Del    int
+	Edit   string
+	Toggle int
+	List   bool
+}
+
+func NewFlags() *Flags {
+	cf := Flags{}
+	flag.StringVar(&cf.Add, "add", "", "Add a new task with title")
+	flag.StringVar(&cf.Edit, "edit", "", "Edit a task by index & set a new title. id:new_title")
+	flag.IntVar(&cf.Del, "del", -1, "Specify task by index to delete")
+	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify task by index to toggle complete true/false")
+	flag.BoolVar(&cf.List, "list", false, "List all tasks")
+
+	flag.Parse()
+
+	return &cf
+}
+
+func (cf *Flags) Execute(todos *Tasks) {
+	switch {
+	case cf.List:
+		todos.print()
+	case cf.Add != "":
+		todos.Add(cf.Add)
+	case cf.Edit != "":
+		parts := strings.SplitN(cf.Edit, ":", 2)
+		if len(parts) != 2 {
+			fmt.Println("Error: Invalid format for edit. Please use index:new_title")
+			os.Exit(1)
+		}
+		index, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error: Invalid index for edit.")
+			os.Exit(1)
+
+		}
+		todos.edit(index, parts[1])
+	case cf.Toggle != -1:
+		todos.toggle(cf.Toggle)
+
+	case cf.Del != -1:
+		todos.Delete(cf.Del)
+
+	default:
+		fmt.Println("Invalid command")
+	}
+}
